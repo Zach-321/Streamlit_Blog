c292926c9af574d2b009dfefd6618af1bc6472a4
Zach Perry <zachp123@byu.edu>
2024-12-04 15:56:05 -0700

    fixed bug

--- my_plots.py
+++ my_plots.py
@@ -9,10 +9,10 @@ import numpy as np
 
 
 def pairs(df, options):
-    if options is None:
-        return 'Please select 2 variables'
-    if len(options)!=2:
-        return 'Please  select 2 variables'
+    # if options is None:
+    #     return 'Please select 2 variables'
+    # if len(options)!=2:
+    #     return 'Please  select 2 variables'
     fig = sns.pairplot(df[options])
     #fig = px.scatter(df,x=options[0],y=options[1])
     return fig
